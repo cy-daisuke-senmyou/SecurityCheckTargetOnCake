95e377f641cce81fea69b02aa52ee5e764dd6025
daisuke-senmyou <daisuke_senmyou@cybird.co.jp>
2016-03-15 14:42:29 +0900

    デバッグログ出力

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -10,9 +10,13 @@ class UsersController extends AppController {
   // Authコンポーネントによる認証
   public function login() {
     if ($this->request->is('post')) {
+      $username = $this->request->data['User']['username'];
+      $password = $this->request->data['User']['password'];
       if ($this->Auth->login()) {
+        $this->log("Login success. [ $username / $password ]", LOG_DEBUG);
         $this->redirect($this->Auth->redirect());
       } else {
+        $this->log("Login failed. [ $username / $password ]", LOG_DEBUG);
         $this->Flash->error(__('Invalid username or password, try again'));
       }
     }
